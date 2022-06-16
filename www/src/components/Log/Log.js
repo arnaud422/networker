7350ad91a25a9d3c71d5e1eaf322d3a75c754c06
arnaud422 <48210106+arnaud422@users.noreply.github.com>
2022-06-16 16:35:12 +0100

    register Firebase

--- www/src/components/Log/Log.js
+++ www/src/components/Log/Log.js
@@ -1,38 +1,45 @@
-import React, { useState } from 'react';
-import SignIn from './SignIn';
-import SignUp from './SignUp';
-
+import React, { useState } from "react";
+import SignIn from "./SignIn";
+import SignUp from "./SignUp";
 
 const Log = (props) => {
-    const [signInModal, setsignInModal] = useState(props.signin)
-    const [signUpModal, setsignUpModal] = useState(props.signup)
+  const [signInModal, setsignInModal] = useState(props.signin);
+  const [signUpModal, setsignUpModal] = useState(props.signup);
 
-    const handleForm = (e)=>{
-        console.log(e.target.id)
-        if(e.target.id === "register"){
-            setsignInModal(false)
-            setsignUpModal(true)
-        }else{
-            setsignInModal(true)
-            setsignUpModal(false)
-        }
+  const handleForm = (e) => {
+    console.log(e.target.id);
+    if (e.target.id === "register") {
+      setsignInModal(false);
+      setsignUpModal(true);
+    } else {
+      setsignInModal(true);
+      setsignUpModal(false);
     }
-    return (
-        <div className='container-log text-init'>
-            <div className="connect">
-                {signInModal && <SignIn/>}
-                {signUpModal && <SignUp/>}
-            </div>
-            <br /><br />
+  };
+  return (
+    <div className="container-log text-init">
+      <div className="connect">
+        {signInModal && <SignIn />}
+        {signUpModal && <SignUp />}
+      </div>
+      <br />
+      <br />
 
-            <div className="footer">
-                <ul>
-                    { (signInModal && !signUpModal) ? <li onClick={handleForm} id="register" className="pointer border">Vous n'avez pas de compte ?</li> : <li onClick={handleForm} id="login" className="pointer">Se connecter</li>}
-                </ul>
-                
-            </div>
-        </div>
-    );
+      <div className="footer">
+
+        {signInModal && !signUpModal ? (
+          <button onClick={handleForm} id="register" className="pointer border">
+            Vous n'avez pas de compte ?
+          </button>
+        ) : (
+          <button onClick={handleForm} id="login" className="pointer">
+            Se connecter
+          </button>
+        )}
+
+      </div>
+    </div>
+  );
 };
 
 export default Log;
--- www/src/components/Log/SignUp.js
+++ www/src/components/Log/SignUp.js
@@ -1,11 +1,59 @@
-import React from 'react';
+import React, { useState } from "react";
+// import { signInWithEmailAndPassword, createUserWithEmailAndPassword, onAuthStateChanged } from "firebase/auth";
+import { createUserWithEmailAndPassword } from "firebase/auth";
+import { auth } from "../../config/firebase";
+
 
 const SignUp = () => {
-    return (
-        <div>
-            coucou depuis register
-        </div>
-    );
+    const [password, setpassword] = useState("")
+    const [rPassword, setrPassword] = useState("")
+    const [mail, setmail] = useState("")
+    const [msgError, setmsgError] = useState("")
+
+    const signUpFireBase = (email, password) => createUserWithEmailAndPassword (auth, email, password)
+
+    const handleData = (e) => {
+        if(e.target.id === 'mail'){
+            setmail(e.target.value)
+        }
+
+        if(e.target.id === 'rPassword'){
+            setrPassword(e.target.value)
+        }
+
+        if(e.target.id === 'password'){
+            setpassword(e.target.value)
+        }
+    }
+
+    const userDataVerifie = async(e) => {
+        e.preventDefault()
+        if(mail && password && rPassword){
+            if(password === rPassword){
+                const cred = await signUpFireBase(mail,password)
+                console.log(cred)
+            }else{
+                setmsgError("Vos mots de passe ne correspondent pas !")
+                return 
+            }
+        }else{
+            setmsgError("Veuillez remplir tous les champs !")
+            return
+        }
+        
+    }
+
+  return (
+    <div className="container-register">
+      <input type="email" id="mail" placeholder="Adresse mail" required onChange={handleData} />
+      <input type="password" id="password" placeholder="Mot de passe" required onChange={handleData}/>
+      <input type="password" id="rPassword" placeholder="Mot de passe" required onChange={handleData}/>
+
+      <button onClick={userDataVerifie}>Créer mon compte</button>
+
+      <span className="text-error">{msgError}</span>
+    </div>
+  );
 };
 
 export default SignUp;
